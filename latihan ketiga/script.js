8e5eb0a9fd31e678ce0f95c7c6b89b1396a63f61
Nama Anda <email@anda.com>
2026-08-11 13:57:50 +0700

    Menambahkan fitur pencarian real-time dan daftar kartu user dari API

--- latihan ketiga/script.js
+++ latihan ketiga/script.js
@@ -1,38 +1,48 @@
-function jalankanPerintah() {
+let daftarPenggunaGlobal = [];
 
-let teksDariUser = document.getElementById("kotakKetikan").value;
+async function muatTim() {
+    let wadah = document.getElementById("wadahPengguna");
+    wadah.innerHTML = "Sedang mengunduh data tim...";
 
-if (teksDariUser === "") {
-    alert("Jangan dikosongkan!");
-    return;
-}
+    try {
+        let respons = await fetch("https://reqres.in/api/users?per_page=6")
+        let hasil = await respons.json();
 
-let barisBaru = document.createElement("li");
+        daftarPenggunaGlobal = hasil.data;
 
-barisBaru.innerHTML = teksDariUser + '❌';
-
-document.getElementById("tempatTugas").appendChild(barisBaru);
-
-let semuaTugas = document.getElementById("tempatTugas").innerHTML;
-localStorage.setItem("dataTugasku", semuaTugas);
-
-document.getElementById("kotakKetikan").value = "";
-
-}
-
-function hapusTugas(tombolYangDiKlik) {
-
-    tombolYangDiKlik.parentElement.remove();
+        tampilkanKeLayar(daftarPenggunaGlobal);
     
-    let semuaTugas = document.getElementById("tempatTugas").innerHTML;
-    
-    localStorage.setItem("dataTugasKu", semuaTugas);
+    }   catch (error) {
+        wadah.innerHTML = "Gagal memuat data.";
+    }
 }
 
-function resetSemua() {
-    
-    localStorage.removeItem("dataTugasKu");
+let wadah = document.getElementById("wadahPengguna");
+wadah.innerHTML ="";
+function tampilkanKeLayar(dataYangMauDitampilkan) {
+    dataYangMauDitampilkan.forEach(function(user) {
+        let kartu = document.createElement("div");
+        kartu.classList.add("kartu-user");
+        kartu.innerHTML = `
+            Foto
 
-    document.getElementById("tempatTugas").innerHTML = "";
+${user.first_name} ${user.last_name}
+
+${user.email}
+
+        `;
+        wadah.appendChild(kartu);
+    });
 }
+function saringData() {
 
+    let ketikanUser = document.getElementById("kotakCari").value.toLowerCase();
+
+    let hasilSaringan = daftarPenggunaGlobal.filter(function(user) {
+        let namaLengkap = user.first_name + " " + user.last_name;
+
+        return namaLengkap.toLocaleLowerCase().includes(ketikanUser);
+    });
+
+    tampilkanKeLayar(hasilSaringan);
+}
